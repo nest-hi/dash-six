8a3d73520b24e5e773687f3aff8447a5dd09d01b
HiImNest <nestbora@gmail.com>
2025-09-18 22:57:30 +0900

    Variables in python

---
+++ Programs/python_practice/variables.py
@@ -0,0 +1,22 @@
+
+# string is a data type that contains a set of characters.
+first_name = 'Diego' # strings must be contained in a pair of single quote ( '' )
+last_name = "Katakutan" # or double quotes ( "" ).
+
+# int contains whole numbers notably,
+age = 13 # positive numbers(+)
+temperature = -39 # negative numbers(-)
+
+# float contain numbers with decimal numbers
+pi = 3.14
+length = 2.1
+
+
+"""
+
+TYPE CASTING:
+an act of specifying a variable's data type
+or replacing its data type on demand.
+
+"""
+
